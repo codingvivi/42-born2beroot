58fe89b6673aa3d096fa9f9632972cadb6791dcf
vivi <135711465+codingvivi@users.noreply.github.com>
2026-03-09 00:27:59 +0100

    Fix install script

--- src/install.sh
+++ src/install.sh
@@ -5,6 +5,6 @@ cp -v ./monitoring.sh /root/.local/bin
 
 chmod -v +x /root/.local/bin/monitoring.sh
 
-cp -v +x 42check.sh /usr/local/bin/42check
+cp -v 42check.sh /usr/local/bin/42check
 
 chmod -v +x /usr/local/bin/42check
