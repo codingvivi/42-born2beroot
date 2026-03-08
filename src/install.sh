7316f3d74e577b46c0eed86e25c7b36cecad8e43
vivi <135711465+codingvivi@users.noreply.github.com>
2026-03-08 23:58:07 +0100

    Add install scripts

---
+++ src/install.sh
@@ -0,0 +1,9 @@
+PS4=$'\n+ '
+
+cp -v ./monitoring.sh /root/.local/bin
+
+chmod -v +x /root/.local/bin/monitoring.sh
+
+cp -v +x 42check.sh /usr/local/bin/42check
+
+chmod -v +x /usr/local/bin/42check
